bdc61b31ffbaedc9bb70951fe312f449086071c3
Sai C <saichaitanya88@gmail.com>
2022-03-15 05:15:21 -0400

    initial draft at the smart contract

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,8 +1,11 @@
 // To conserve gas, efficient serialization is achieved through Borsh (http://borsh.io/)
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::UnorderedMap;
+use near_sdk::collections::{LookupMap, UnorderedMap};
+use near_sdk::json_types::U128;
 use near_sdk::serde_json::json;
-use near_sdk::{env, near_bindgen, setup_alloc, assert_one_yocto};
+use near_sdk::{assert_one_yocto, env, near_bindgen, setup_alloc, AccountId, Balance};
+use std::collections::HashMap;
+
 
 use crate::models::*;
 use crate::utils::*;
@@ -18,12 +21,14 @@ setup_alloc!();
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct NearBasicAttentionToken {
   ad_campaigns: UnorderedMap<String, AdCampaign>,
+  ad_campaign_impressions: UnorderedMap<String, AdCampaignImpression>,
 }
 
 impl Default for NearBasicAttentionToken {
   fn default() -> Self {
     Self {
       ad_campaigns: UnorderedMap::new(StorageKey::AdCampaign),
+      ad_campaign_impressions: UnorderedMap::new(StorageKey::AdCampaignImpression),
     }
   }
 }
@@ -31,8 +36,113 @@ impl Default for NearBasicAttentionToken {
 #[near_bindgen]
 impl NearBasicAttentionToken {
   #[payable]
-  pub create_ad_campaign(&mut self, ad_campaign: AdCampaign) -> String {
-    assert_one_yocto();
+  pub fn create_ad_campaign(&mut self, ad_campaign_in: AdCampaign) -> String {
+    // Assert
+    assert!(env::attached_deposit() > 1, "Deposit required");
+    // Basic validations
+    assert!(ad_campaign_in.min_impressions < ad_campaign_in.max_impressions);
+
+    // check id is in format `owner_account_id:ad_campaign_id`
+    let formatted_id_starts_with = format!("{}:", env::signer_account_id());
+    assert!(ad_campaign_in.id.contains(&formatted_id_starts_with));
+
+    let existng_ad_campaign = self.ad_campaigns.get(&ad_campaign_in.id);
+    assert!(
+      existng_ad_campaign.is_none(),
+      "Store with ID already exists"
+    );
+
+    // Apply
+    let mut ad_campaign = ad_campaign_in;
+    ad_campaign.owner_account_id = env::signer_account_id();
+    ad_campaign.amount = env::attached_deposit();
+    ad_campaign.status = AdCampaignStatus::ACTIVE;
+    ad_campaign.amount_per_impression =
+      env::attached_deposit() as u128 / ad_campaign.max_impressions as u128;
+
+    // Add
+    self.ad_campaigns.insert(&ad_campaign.id, &ad_campaign);
+    let ad_campaign_impressions = &AdCampaignImpression {
+      ad_campaign_id: ad_campaign.id,
+      impressions: HashMap::new(),
+    };
+    self.ad_campaign_impressions.insert(
+      &ad_campaign_impressions.ad_campaign_id,
+      &ad_campaign_impressions,
+    );
+    String::from("OK")
+  }
+
+  pub fn increment_impression(&mut self, ad_campaign_id: String) -> String {
+    // Check AdCampaign exists
+    let ad_campaign = self
+      .ad_campaigns
+      .get(&ad_campaign_id)
+      .expect("AdCampaign does not exist");
+
+    // Can't do anything if AdCampaign is completed
+    if matches!(ad_campaign.status, AdCampaignStatus::COMPLETED) {
+      return String::from("OK");
+    }
+
+    let mut existing_ad_campaign_impression = self
+      .ad_campaign_impressions
+      .get(&String::from(format!(
+        "{}:{}",
+        ad_campaign_id,
+        env::signer_account_id()
+      )))
+      .expect("AdCampaignImpression does not exist");
+
+    let _impressions = existing_ad_campaign_impression
+      .impressions.entry(env::signer_account_id()).or_insert(0);
+    *_impressions += 1;
+    let impressions = *_impressions;
+
+    // Update this impression
+    existing_ad_campaign_impression
+      .impressions
+      .insert(env::signer_account_id(), impressions);
+
+    // Update total impressions
+    let mut total_impressions = 0;
+    for impression in existing_ad_campaign_impression.impressions.values() {
+      total_impressions += impression
+    }
+
+    if total_impressions > ad_campaign.min_impressions {
+      self.close_ad_campaign(
+        ad_campaign_id,
+        total_impressions,
+        existing_ad_campaign_impression.impressions,
+      );
+    }
+
+    String::from("OK")
+  }
+
+  pub fn close_ad_campaign(
+    &mut self,
+    ad_campaign_id: String,
+    total_impressions: u8,
+    existing_ad_campaign_impressions: HashMap<AccountId, u8>,
+  ) -> String {
+    let mut ad_campaign = self
+      .ad_campaigns
+      .get(&ad_campaign_id)
+      .expect("AdCampaign is not found");
+    // set ad_campaign.status to CLOSED
+    ad_campaign.status = AdCampaignStatus::COMPLETED;
+    self.ad_campaigns.insert(&ad_campaign_id, &ad_campaign);
+
+    // Payout the ad_campaign members
+    let payout_per_viewer = ad_campaign.amount / total_impressions as u128;
+    // loop through, i'm sure there's a better way
+    for account_id in existing_ad_campaign_impressions.keys() {
+      near_sdk::Promise::new(AccountId::from(account_id)).transfer(payout_per_viewer);
+    }
+
+    String::from("OK")
   }
 }
 
@@ -77,28 +187,90 @@ mod tests {
 
   #[test]
   fn test_create_ad_campaign() {
-    let context = get_context(vec![], false);
+    let mut context = get_context(vec![], false);
+    let attached_deposit = ntoy(1);
+    context.attached_deposit = attached_deposit;
     testing_env!(context);
     let mut contract = NearBasicAttentionToken::default();
-    let ad_campaign_data = json!({
-      "name": "Campaign Name",
-      "cta": "https://google.com",
-      "description": "Some campaign description",
-      "Categories": ["Technology", "Biology"],
-      "amount": ntoy(1),
-      "min_impressions": 100,
-      "max_impressions": 110,
-    })
-    .to_string();
+    let ad_campaign_data = AdCampaign {
+      name: String::from("Campaign Name"),
+      cta: String::from("https://google.com"),
+      description: String::from("Some campaign description"),
+      categories: vec![String::from("Technology"), String::from("Biology")],
+      amount: attached_deposit,
+      min_impressions: u8::from(100),
+      max_impressions: u8::from(110),
+      owner_account_id: String::from("bob_near"),
+      amount_per_impression: 0 as u128,
+      id: format!("{}:{}", String::from("bob_near"), "0"),
+      status: AdCampaignStatus::ACTIVE,
+    };
+
     assert_eq!(
-      "0".to_string(),
+      "OK".to_string(),
       contract.create_ad_campaign(ad_campaign_data)
     );
   }
 
   // increment_impression
+  fn test_increment_impression() {
+    let mut context = get_context(vec![], false);
+    let attached_deposit = ntoy(1);
 
-  // close_ad_campaign
+    testing_env!(context);
+    let mut contract = NearBasicAttentionToken::default();
+    let ad_campaign_id = format!("{}:{}", "near_test", "0");
+    let ad_campaign_data = AdCampaign {
+      name: String::from("Campaign Name"),
+      cta: String::from("https://google.com"),
+      description: String::from("Some campaign description"),
+      categories: vec![String::from("Technology"), String::from("Biology")],
+      amount: attached_deposit,
+      min_impressions: u8::from(100),
+      max_impressions: u8::from(110),
+      owner_account_id: String::from("bob_near"),
+      amount_per_impression: 0 as u128,
+      id: format!("{}:{}", String::from("bob_near"), "0"),
+      status: AdCampaignStatus::ACTIVE,
+    };
+    contract
+      .ad_campaigns
+      .insert(&ad_campaign_id, &ad_campaign_data);
+    assert_eq!(
+      String::from("OK"),
+      contract.increment_impression(ad_campaign_id)
+    );
+  }
 
-  // payout
+  // close_ad_campaign and payout
+  fn test_close_ad_campaign() {
+    let mut context = get_context(vec![], false);
+    let attached_deposit = ntoy(1);
+
+    testing_env!(context);
+    let mut contract = NearBasicAttentionToken::default();
+    let ad_campaign_id = format!("{}:{}", "near_test", "0");
+    let ad_campaign_data = AdCampaign {
+      name: String::from("Campaign Name"),
+      cta: String::from("https://google.com"),
+      description: String::from("Some campaign description"),
+      categories: vec![String::from("Technology"), String::from("Biology")],
+      amount: attached_deposit,
+      min_impressions: u8::from(0),
+      max_impressions: u8::from(1),
+      owner_account_id: String::from("bob_near"),
+      amount_per_impression: 0 as u128,
+      id: format!("{}:{}", String::from("bob_near"), "0"),
+      status: AdCampaignStatus::ACTIVE,
+    };
+    contract
+      .ad_campaigns
+      .insert(&ad_campaign_id, &ad_campaign_data);
+
+    contract.increment_impression(ad_campaign_id); // This should trigger campaign to be closed
+    assert!(matches!(
+      contract.ad_campaigns.get(&format!("{}:{}", "near_test", "0")).expect("").status,
+      AdCampaignStatus::COMPLETED
+    ));
+  }
 }
--- contract/src/models.rs
+++ contract/src/models.rs
@@ -1,24 +1,43 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::UnorderedMap;
 use near_sdk::json_types::{U128, U64};
 use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::BorshStorageKey;
+use near_sdk::{AccountId, Balance};
+use std::collections::HashMap;
 
 /// Helper structure to for keys of the persistent collections.
 #[derive(BorshStorageKey, BorshSerialize)]
 pub enum StorageKey {
-    AdCampaign
+    AdCampaign,
+    AdCampaignImpression,
+    AdCampaignImpressionPerViewer,
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub enum AdCampaignStatus {
+    ACTIVE, COMPLETED
 }
 
 #[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct AdCampaign {
     pub id: String,
+    pub owner_account_id: String,
     pub name: String,
     pub cta: String, // navigate to a url
     pub description: String,
     pub categories: Vec<String>, // eg. Technology, Medicine, Arts, Politics, Biology etc.
-    pub amount: U128,
-    pub amount_per_impression: U128, // for estimation only
-    pub min_impressions: U64,        // example assumes less than 128 impressions
-    pub max_impressions: U64,        // example assumes less than 128 impressions
+    pub amount: u128,
+    pub amount_per_impression: u128, // for estimation only
+    pub min_impressions: u8,         // example assumes less than 128 impressions
+    pub max_impressions: u8,         // example assumes less than 128 impressions
+    pub status: AdCampaignStatus
+}
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct AdCampaignImpression {
+    pub ad_campaign_id: String, // formatted to ad_campaign_id:impression_account_id
+    pub impressions: HashMap<AccountId, u8>, 
 }
